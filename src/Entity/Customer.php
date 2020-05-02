f27dcf6c7f280320070f4e83b10e89d6e8ba14d3
mouctare <mouctard78@gmail.com>
2020-05-02 11:00:40 +0200

    AddModiff

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Invoice;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CustomerRepository")
+ * @ApiResource // Cette anotation permet de rendre cette entité visible
  */
 class Customer
 {
@@ -15,6 +18,7 @@ class Customer
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * 
      */
     private $id;
 
