841f22314b02b645737f4a295d8c851bf22c018f
mouctare <mouctard78@gmail.com>
2020-05-12 14:49:48 +0200

    Ajout de la page customer plus les requete get et delete

---
+++ assets/js/pages/CustomersPage.jsx
@@ -0,0 +1,78 @@
+import React , { useEffect , useState}from 'react';
+import axios from "axios";
+
+ const CustomersPage = props =>{
+
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/customers")
+        .then(response => response.data[ 'hydra:member'])
+        .then(data => setCustomers(data))
+        .catch(error=> console.log(error.response));
+        }, []);
+
+        const handleDelete = (id) => {
+            const originalCustomers = [...customers];
+            // 1. L'approche optimiste
+            setCustomers(customers.filter(customer => customer.id !== id));
+            // 2. L'approche pessimiste
+            axios
+            .delete("http://localhost:8000/api/customers/" + id) // çava supprimer ce customer.
+            .then(response => console.log("ok"))
+            .catch(error => {
+                setCustomers(originalCustomers);
+                console.log(error.response);
+            });
+        };
+
+      return (
+        <>
+        <h1>Listes des clients</h1>
+
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                <th>Id.</th>
+                <th>Client</th>
+                <th>Email</th>
+                <th>Entrprise</th>
+                <th className="text-center">Factures</th>
+                <th className="text-center">Montant total</th>
+                <th></th>
+            </tr>
+            </thead>
+
+            <tbody>
+                {customers.map(customer =>  
+                <tr key={customer.id}>
+                <td>{customer.id}</td>
+                <td>
+                 <a href="#">{customer.firstName} {customer.lastName}</a>
+                </td>
+                <td>{customer.email}</td>
+                <td>{customer.compagny}</td>
+                <td className="text-center">
+                <span className="badge badge-primary">{customer.invoices.length}</span>
+                </td>
+                <td className="text-center">{customer.totalAmount.toLocaleString()} €</td>
+                <td>
+                    <button 
+                    onClick={() => handleDelete(customer.id)} // Quand on clik sur ce boutton on va applé cette function.
+                    disabled={customer.invoices.length > 0}
+                    className="btn btn-sm btn-danger"
+                    >
+                        Supprimer
+                        </button> 
+                </td>
+               </tr>)}
+               
+            </tbody>
+        </table>
+
+        </>
+    );
+}
+ 
+
+export default CustomersPage;
