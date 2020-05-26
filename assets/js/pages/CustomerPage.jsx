2a7453de5a017ca06e8b11888077e1474788af5b
mouctare <mouctard78@gmail.com>
2020-05-26 13:03:33 +0200

    Ajout des contraints de valudation

--- assets/js/pages/CustomerPage.jsx
+++ assets/js/pages/CustomerPage.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {Link} from "react-router-dom";
 import Field from "../components/forms/Field";
 import axios from "axios";
@@ -6,20 +6,51 @@ import axios from "axios";
 
 
 const CustomerPage = (props) => {
+    // Je vais sortir l'identifiant de mes params.
+    const { id = "new" } = props.match.params;
+
     const [customer, setCustomer]  = useState({
-        lastName: "Chamla",
+        lastName: "",
         firstName: "",
         email: "",
-        compagny: ""
+        company: ""
     });
-
+   
     const [errors, setErrors] = useState({
         lastName: "",
         firstName: "",
         email: "",
-        compagny: ""
-
+        company: ""
+        
     });
+    
+    const [editing, setEditing] = useState(false);
+
+    const fetchCustomer = async id => {
+        try {
+            const data =  await axios
+            .get("http://localhost:8000/api/customers/" + id) 
+            .then(response => response.data);
+            const { firstName,lastName,email, company} = data;
+
+            setCustomer({ firstName, lastName,email,company});
+         } catch (error) {
+             console.log(error.response);
+         }
+            
+    }
+
+
+
+    useEffect(() =>{
+        // Si id est différent de new, je suis entrain d'editer
+        if(id !== "new") {
+        setEditing(true);
+        fetchCustomer(id);
+       }
+
+    }, [id]);
+
 
     const handleChange = ({currentTarget}) => {
         const {name, value} = currentTarget;
@@ -30,18 +61,46 @@ const CustomerPage = (props) => {
         event.preventDefault();
 
         try {
-           const response = await axios.post("http://localhost:8000/api/customers", customer)
-           console.log(response.data);
 
-        }catch(error) {
-            console.log(error.response);
+            if(editing) {
+                // Si je suis en mode édition, je vais applé cette methode put
+                const response = await axios.put(
+                    "http://localhost:8000/api/customers/" + id, 
+                    customer
+                    );
+                
+                    // TODO : flash notification de succés
+            } else {
+                // Si je suis pas en mode edition , 
+                // je vais applé la methode post car dans ce cas je suis entrain de créer un customer
+                const response = await axios.post(
+                    "http://localhost:8000/api/customers", 
+                customer
+                );
+              // Quand on ajoute quelquin , on fait une redirection vers les customers
+              // TODO : flash notification de succés
+              props.history.replace("/customers");
+
+            }
+
+           setErrors({});
+            }catch(error) {
+           if(error.response.data.violations) {
+               const apiErros = {};
+               error.response.data.violations.forEach(violation => {
+                   apiErros[violation.propertyPath] = violation.message;
+                   
+               });
+               setErrors(apiErros);
+               // TODO : flash notification de succés
+           }
         }
         
     }
 
     return (
         <div className="container">
-              <h1>Création d'un client</h1>
+             {!editing && <h1>Création d'un client</h1> || <h1>Modification du client</h1>}
 
               <form onSubmit={handleSubmit}>
                  <Field 
@@ -73,9 +132,9 @@ const CustomerPage = (props) => {
                   name="compagny" 
                   label="Entrprise" 
                   placelholder="Entreprise du client" 
-                  value={customer.compagny}
+                  value={customer.company}
                   onChange={handleChange}
-                  error={errors.compagny}
+                  error={errors.company}
                    />
                  <div className="form-group">
                      <button type="submit" className="btn btn-success">Enregistrer</button>
@@ -88,6 +147,7 @@ const CustomerPage = (props) => {
     
     </div>
     );
+    
 
 };
 
