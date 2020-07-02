02109ad46bd1129ab080e8c89a5a01349ce709e3
mouctare <mouctard78@gmail.com>
2020-07-02 16:38:29 +0200

    homepage modifie

--- assets/js/components/HomePages.jsx
+++ assets/js/components/HomePages.jsx
@@ -8,7 +8,7 @@ import React from 'react'
     return (
         <>
           <div className="container">
-            <h1 className="display-4">Hello, world!</h1>
+            <h1 className="display-4">Hello, world!  symRect à votre service </h1>
         <p className="lead">This is a simple hero unit, a simple jumbotron-style component
          for calling extra attention to featured content or information.</p>
         <hr className="my-4"/>
